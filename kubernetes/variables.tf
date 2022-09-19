e200970a709a255c38fd9bed2fc2d6ceba718ccb
LordSAK <3930868+LordSAK@users.noreply.github.com>
2022-09-19 14:49:19 +0500

    First commit

---
+++ kubernetes/variables.tf
@@ -0,0 +1,97 @@
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "API_PUBLIC_ACCESS" {
+  default     = true
+  type        = bool
+  description = "Allow api server to be accessed using public endpoint."
+}
+
+variable "API_PRIVATE_ACCESS" {
+  type        = bool
+  default     = true
+  description = "Allow API server to be accessed using private endpoint."
+}
+
+variable "ENABLED_CLUSTER_LOG_TYPES" {
+  default = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+}
+
+variable "CLUSTER_LOG_RETENTATION_IN_DAYS" {
+  default = 7
+}
+# service.beta.kubernetes.io/aws-load-balancer-internal: "true"
+
+variable "INSTANCE_TYPES" {
+  type        = list(string)
+  description = "List of Instance types to create the worker nodegroup."
+  default     = ["t2.medium"]
+}
+
+variable "VPC_ID" {
+  type        = string
+  description = "VPC ID of the VPC where cluster should be."
+  default     = "vpc-009a103bb8eea14d9"
+}
+
+variable "CLUSTER_NAME" {
+  type        = string
+  description = "Name of the EKS cluster."
+  default     = "EKS_CLUSTER"
+}
+
+variable "CLUSTER_VERSION" {
+  default     = "1.20"
+  type        = string
+  description = "Kubernetes version in EKS."
+}
+
+variable "ROOT_VOLUME_TYPE" {
+  default     = "standard"
+  type        = string
+  description = "AWS volume type to use for the Root volumes of the nodes in nodegroup."
+}
+
+variable "ROOT_VOLUME_SIZE" {
+  default     = 50
+  type        = number
+  description = "Size of the root volume of the worker nodes."
+}
+
+variable "MAX_SIZE" {
+  default     = 4
+  type        = number
+  description = "Maximum number of worker nodes."
+}
+
+variable "MIN_SIZE" {
+  default     = 2
+  type        = number
+  description = "Minimum Number of worker nodes."
+}
+
+variable "DESIRED_SIZE" {
+  default     = 2
+  type        = string
+  description = "Desired capacity of the worker nodes."
+}
+
+variable "FORCE_DELETE" {
+  default = false
+}
+
+variable "WORKERS_SUBNETS" {
+  type        = list(string)
+  description = "List of the subnets on which the worker nodes will be on."
+  default     = ["subnet-0e6f01edc1d7c4028", "subnet-0f2a9e5e4b5fb9671"]
+}
+
+variable "API_SUBNET" {
+  type        = list(string)
+  description = "List of Subnet on which the eks api server will be on."
+  default     = ["subnet-0e6f01edc1d7c4028", "subnet-0f2a9e5e4b5fb9671"]
+}
